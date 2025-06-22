35b5237d8feb9bd0f700666126808a85bfc7fe9c
Dawit Moges <153367606+kenji-villa@users.noreply.github.com>
2025-06-22 11:55:15 +0300

    Update Details

---
+++ UpdateTaxi.java
@@ -0,0 +1,51 @@
+package Taxi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UpdateTaxi {
+	private static final String DB_URL = "jdbc:sqlserver://LENO\\SQLEXPRESS;databaseName=Adminstors;integratedSecurity=true;encrypt=false;portNumber=1433";
+
+    public boolean updateTaxi(String oldTaxiID, AddTaxii newTaxi) {
+        String searchQuery = "SELECT * FROM Taxi WHERE TaxiID = ?";
+        String updateQuery = "UPDATE Taxi SET TaxiID = ?, Capacity = ?, Model = ?, Year = ? WHERE TaxiID = ?";
+
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            try (Connection connection = DriverManager.getConnection(DB_URL);
+                 PreparedStatement searchStmt = connection.prepareStatement(searchQuery);
+                 PreparedStatement updateStmt = connection.prepareStatement(updateQuery)) {
+
+                // Search for the taxi by old TaxiID
+                searchStmt.setString(1, oldTaxiID);
+                ResultSet resultSet = searchStmt.executeQuery();
+
+                if (resultSet.next()) {
+                    // Taxi found, update the details
+                    updateStmt.setString(1, newTaxi.getTaxiID());
+                    updateStmt.setInt(2, newTaxi.getCapacity());
+                    updateStmt.setString(3, newTaxi.getModel());
+                    updateStmt.setInt(4, newTaxi.getYear());
+                    updateStmt.setString(5, oldTaxiID);
+
+                    int rowsUpdated = updateStmt.executeUpdate();
+                    return rowsUpdated > 0;
+                } else {
+                    System.out.println("Taxi with ID " + oldTaxiID + " not found.");
+                    return false;
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
